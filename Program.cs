243bd29b9071590eac1a2e5502b52fb309e600f6
IrenaStonkiene <irena.stonkiene@hotmail.com>
2022-02-27 22:08:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name = "Spider";
+int age = 23;
+float hight = 1.9f;
+string gender = "M";
+string superpower1 = "Can fly";
+string superpower2 = "Speaks 20 languages";
+string superpower3 = "can be invisable";
+
+Console.WriteLine("*************************");
+Console.WriteLine($"Hero: {name}");
+Console.WriteLine($"Hight: {hight}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Gender: {gender}");
+Console.WriteLine($"Superpowers: {superpower1}, {superpower2}, {superpower3}") ;
+Console.WriteLine("*************************");
+
+
+
